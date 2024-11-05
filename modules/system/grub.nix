3a9947f78cbf6be2215df852410978bad148be11
Igor Rzegocki <igor@rzegocki.pl>
2024-11-05 16:12:28 +0100

    feat: add grub

---
+++ modules/system/grub.nix
@@ -0,0 +1,33 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.mySystem.grub;
+  grubTheme = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "grub";
+    rev = "v1.0.0";
+    sha256 = "sha256-/bSolCta8GCZ4lP0u5NVqYQ9Y3ZooYCNdTwORNvR7M0=";
+  };
+in
+{
+  options.mySystem.grub = {
+    enable = lib.mkEnableOption "grub bootloader";
+  };
+
+  config = lib.mkIf cfg.enable {
+    boot.loader = {
+      systemd-boot.enable = false;
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
+        useOSProber = true;
+        theme = "${grubTheme}/src/catppuccin-mocha-grub-theme";
+      };
+    };
+  };
+}
