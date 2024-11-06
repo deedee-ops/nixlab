0b50197de2ca8dcc219eb8bec8a0578a9adc847f
Igor Rzegocki <igor@rzegocki.pl>
2024-11-06 15:38:41 +0100

    fix(ssh): move known hosts to state directory

--- modules/apps/ssh/default.nix
+++ modules/apps/ssh/default.nix
@@ -25,10 +25,19 @@ in
       package = pkgs.openssh;
 
       addKeysToAgent = "8h";
+      userKnownHostsFile = "${config.xdg.stateHome}/ssh/known_hosts";
     } cfg.appendOptions;
 
     services.ssh-agent = {
       enable = true;
+    };
+
+    home = {
+      activation = {
+        ssh = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          run mkdir -p ${config.xdg.stateHome}/ssh || true
+        '';
+      };
     };
   };
 }
