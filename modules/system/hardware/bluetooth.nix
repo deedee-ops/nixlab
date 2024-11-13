fdf287a195908ab28e1d8aa723afe62d93dc5e90
Igor Rzegocki <igor@rzegocki.pl>
2024-11-13 23:56:03 +0100

    feat(bluetooth): support default trusted devices, and wake from suspend

--- modules/system/hardware/bluetooth.nix
+++ modules/system/hardware/bluetooth.nix
@@ -10,9 +10,41 @@ in
 {
   options.myHardware.bluetooth = {
     enable = lib.mkEnableOption "bluetooth";
+    trust = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      description = "List of bluetooth devices mac addresses to be automatically trusted.";
+      default = [ ];
+      example = [ "AA:BB:CC:DD:EE:FF" ];
+    };
+    wakeFromSuspend = lib.mkOption {
+      type = lib.types.submodule {
+        options = {
+          enable = lib.mkEnableOption "wake up from suspend";
+          vendorID = lib.mkOption {
+            type = lib.types.nullOr lib.types.str;
+            description = "Vendor ID of bluetooth controller.";
+          };
+          productID = lib.mkOption {
+            type = lib.types.nullOr lib.types.str;
+            description = "Product ID of bluetooth controller.";
+          };
+        };
+      };
+    };
   };
 
   config = lib.mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = (!cfg.wakeFromSuspend.enable) || (cfg.wakeFromSuspend.vendorID != null);
+        message = "When wakeFromSuspend is enabled, vendorID must be set.";
+      }
+      {
+        assertion = (!cfg.wakeFromSuspend.enable) || (cfg.wakeFromSuspend.productID != null);
+        message = "When wakeFromSuspend is enabled, productID must be set.";
+      }
+    ];
+
     hardware.bluetooth = {
       enable = true;
       powerOnBoot = true;
@@ -25,26 +57,36 @@ in
       # };
     };
 
-    services.blueman.enable = true;
+    services = {
 
-    services.pipewire.wireplumber.extraConfig.bluetoothEnhancements =
-      lib.mkIf config.myHardware.sound.enable
-        {
-          "monitor.bluez.properties" = {
-            "bluez5.enable-sbc-xq" = true;
-            "bluez5.enable-msbc" = true;
-            "bluez5.enable-hw-volume" = true;
-            "bluez5.autoswitch-profile" = true;
-            "bluez5.roles" = [
-              "hsp_hs"
-              "hsp_ag"
-              "hfp_hf"
-              "hfp_ag"
-              "a2dp_sink"
-            ];
-          };
+      blueman.enable = true;
+
+      pipewire.wireplumber.extraConfig.bluetoothEnhancements = lib.mkIf config.myHardware.sound.enable {
+        "monitor.bluez.properties" = {
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-hw-volume" = true;
+          "bluez5.autoswitch-profile" = true;
+          "bluez5.roles" = [
+            "hsp_hs"
+            "hsp_ag"
+            "hfp_hf"
+            "hfp_ag"
+            "a2dp_sink"
+          ];
         };
+      };
 
+      udev.extraRules = lib.mkIf cfg.wakeFromSuspend.enable ''
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="8087", ATTRS{idProduct}=="0aaa" RUN+="/bin/sh -c 'echo enabled > /sys$env{DEVPATH}/../power/wakeup;'
+      '';
+    };
     homeApps.awesome.autorun = [ (lib.getExe' pkgs.blueman "blueman-applet") ];
+
+    system.activationScripts = {
+      bluez-autotrust = lib.concatStringsSep "\n" (
+        builtins.map (trusted: "${lib.getExe' pkgs.bluez "bluetoothctl"} trust ${trusted}") cfg.trust
+      );
+    };
   };
 }
