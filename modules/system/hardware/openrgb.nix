906747b96635cda41d2177041d2e5736749aedfe
Igor Rzegocki <igor@rzegocki.pl>
2024-12-18 19:39:45 +0100

    fix(openrgb): persist user config

--- modules/system/hardware/openrgb.nix
+++ modules/system/hardware/openrgb.nix
@@ -18,12 +18,20 @@ in
 
   config = lib.mkIf cfg.enable {
     services.hardware.openrgb.enable = true;
+    boot.kernelParams = [ "acpi_enforce_resources=lax" ]; # g.skill RAM has problems when this is not set
 
-    systemd.services.apply-openrgb-profile = lib.mkIf (cfg.profile != null) {
-      script = ''
-        ${lib.getExe config.services.hardware.openrgb.package} -p ${cfg.profile}
-      '';
-      wantedBy = [ "multi-user.target" ];
+    home-manager.users."${config.mySystem.primaryUser
+    }".home.persistence."${config.mySystem.impermanence.persistPath}${
+      config.home-manager.users."${config.mySystem.primaryUser}".home.homeDirectory
+    }" =
+      lib.mkIf config.mySystem.impermanence.enable {
+        directories = [
+          ".config/OpenRGB"
+        ];
+      };
+
+    mySystemApps.xorg.userAutorun = {
+      apply-openrgb-profile = "${lib.getExe config.services.hardware.openrgb.package} -p ${cfg.profile}";
     };
   };
 }
