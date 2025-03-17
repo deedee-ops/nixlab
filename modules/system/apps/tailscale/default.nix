5547ddb4d676a018634b50eb3e58e3259e660fcb
Igor Rzegocki <igor@rzegocki.pl>
2025-03-17 20:49:17 +0100

    fix(tailscale): configurable autoprovisioning

--- modules/system/apps/tailscale/default.nix
+++ modules/system/apps/tailscale/default.nix
@@ -5,6 +5,7 @@ in
 {
   options.mySystemApps.tailscale = {
     enable = lib.mkEnableOption "tailscale";
+    autoProvision = lib.mkEnableOption "auto provision with auth key";
     authKeySopsSecret = lib.mkOption {
       type = lib.types.str;
       description = "Sops secret name containing auth key.";
@@ -23,17 +24,21 @@ in
       # restartUnits = ["tailscaled-autoconnect"];
     };
 
-    services.tailscale = {
-      enable = true;
-      authKeyFile = config.sops.secrets."${cfg.authKeySopsSecret}".path;
-      disableTaildrop = true;
-      extraUpFlags =
-        [
-          "--accept-dns=false"
-        ]
-        ++ (lib.optionals (builtins.length cfg.advertiseRoutes > 0) [
-          ("--advertise-routes=" + (builtins.concatStringsSep "," cfg.advertiseRoutes))
-        ]);
-    };
+    services.tailscale =
+      {
+        enable = true;
+        disableTaildrop = true;
+      }
+      // lib.optionalAttrs cfg.autoProvision {
+        authKeyFile = config.sops.secrets."${cfg.authKeySopsSecret}".path;
+        extraUpFlags =
+          [
+            "--accept-dns=false"
+          ]
+          ++ (lib.optionals (builtins.length cfg.advertiseRoutes > 0) [
+            ("--advertise-routes=" + (builtins.concatStringsSep "," cfg.advertiseRoutes))
+          ]);
+
+      };
   };
 }
