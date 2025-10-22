5ac2de450d95e9c2622c3582fbffd1a1553a8abb
Igor Rzegocki <igor@rzegocki.pl>
2025-10-22 23:23:44 +0200

    fix(bazarr): refresh jellyfin after subtitles download

--- modules/system/containers/bazarr/refresh-jellyfin.sh
+++ modules/system/containers/bazarr/refresh-jellyfin.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 echo "INFO : Refreshing jellyfin ..."
-curl -X POST "${JELLYFIN_URL}/library/refresh?api_key=$(cat /secrets/JELLYFIN_API_KEY)"
+curl -X POST "${JELLYFIN_URL}/library/refresh?api_key=${JELLYFIN_API_KEY}"
