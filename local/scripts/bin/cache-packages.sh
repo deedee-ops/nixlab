a87b42fbf4364c98db4e1f10b1fb8b4ee7e9db18
Igor Rzegocki <igor@rzegocki.pl>
2025-11-22 16:17:29 +0100

    chore(actions): switch to kubernetes runner

--- local/scripts/bin/cache-packages.sh
+++ local/scripts/bin/cache-packages.sh
@@ -5,17 +5,25 @@ for sub in $(nix --accept-flake-config eval --json '.#nixlab.nixConfig.substitut
   for machine in $(nix --accept-flake-config flake show --json 2> /dev/null | jq -r '.nixosConfigurations | keys | .[]'); do
     drv=".#nixosConfigurations.$machine.config.system.build.toplevel"
 
-    echo "Building and caching ${machine}..."
+    printf "\033[1;91mBuilding and caching %s...\033[0m\n" "${machine}"
 
-    nom build --accept-flake-config --fallback --no-link "$drv"
+    if [ -n "$CI" ]; then
+      nix build --accept-flake-config --fallback --no-link "$drv"
+    else
+      nom build --accept-flake-config --fallback --no-link "$drv"
+    fi
     nix --accept-flake-config store sign --key-file <(echo "${NIXCACHE_PRIVATE_KEY}") --recursive "$drv"
     nix --accept-flake-config store verify --sigs-needed 1 --recursive "$drv" --option trusted-public-keys "${NIXCACHE_PUBLIC_KEY}"
     nix --accept-flake-config --refresh copy --to "$s3sub" "$drv"
   done
 
   drv=".#devShells.x86_64-linux.default"
-  echo "Building and caching devshell..."
-  nom build --accept-flake-config --fallback --no-link "$drv"
+  printf "\033[1;91mBuilding and caching devshell...\033[0m\n"
+  if [ -n "$CI" ]; then
+    nix build --accept-flake-config --fallback --no-link "$drv"
+  else
+    nom build --accept-flake-config --fallback --no-link "$drv"
+  fi
   nix --accept-flake-config store sign --key-file <(echo "${NIXCACHE_PRIVATE_KEY}") --recursive "$drv"
   nix --accept-flake-config store verify --sigs-needed 1 --recursive "$drv" --option trusted-public-keys "${NIXCACHE_PUBLIC_KEY}"
   nix --accept-flake-config --refresh copy --to "$s3sub" "$drv"
