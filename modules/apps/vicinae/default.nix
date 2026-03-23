47c8b86a5e67c233f2ebdf17cb8e11248392248c
Igor Rzegocki <igor@rzegocki.pl>
2026-03-23 19:54:19 +0100

    fix(vicinae): disable telemetry

--- modules/apps/vicinae/default.nix
+++ modules/apps/vicinae/default.nix
@@ -161,6 +161,9 @@ in
           escape_key_behavior = "close_window";
           pop_to_root_on_close = true;
           launcher_window.opacity = 1.0;
+          telemetry = {
+            system_info = false;
+          };
           theme = {
             dark.name = "stylix";
             light.name = "stylix";
