52480b46e5fc2936a536e97be0abd1643c23631e
Igor Rzegocki <igor@rzegocki.pl>
2026-04-25 16:53:17 +0200

    feat: support noctalia themes in terminal apps

--- modules/features/home/yazi/default.nix
+++ modules/features/home/yazi/default.nix
@@ -1,60 +1,61 @@
-{ self, inputs, ... }:
-{
+_: {
   flake.homeModules.features-home-yazi =
-    { pkgs, ... }:
+    { config, lib, ... }:
     {
-      stylix.targets.yazi.enable = true;
+      stylix.targets.yazi.enable = !config.programs.noctalia-shell.enable;
+      programs.noctalia-shell.settings.templates.activeTemplates = [
+        {
+          enabled = true;
+          id = "yazi";
+        }
+      ];
 
       programs.yazi = {
         enable = true;
-        package = self.packages."${pkgs.stdenv.hostPlatform.system}".yazi;
         shellWrapperName = "y";
-      };
-    };
-  perSystem =
-    { pkgs, ... }:
-    {
-      packages.yazi = inputs.wrapper-modules.wrappers.yazi.wrap {
-        inherit pkgs;
 
-        settings = {
-          keymap = {
-            mgr = {
-              prepend_keymap = [
-                {
-                  on = "d";
-                  run = "remove --permanently";
-                  desc = "Remove permanently.";
-                }
-              ];
-            };
+        keymap = {
+          mgr = {
+            prepend_keymap = [
+              {
+                on = "d";
+                run = "remove --permanently";
+                desc = "Remove permanently.";
+              }
+            ];
           };
-          theme = {
-            indicator = {
-              preview = {
-                underline = false;
-              };
-            };
-          };
-          yazi = {
-            mgr = {
-              ratio = [
-                1
-                3
-                6
-              ];
-              sort_by = "alphabetical";
-              sort_sensitive = false;
-              sort_dir_first = true;
-              show_hidden = true;
-              show_symlink = true;
-            };
+        };
+        theme = {
+          indicator = {
             preview = {
-              wrap = "yes";
-              tab_size = 2;
-              max_width = 2700;
-              max_height = 2050;
+              underline = false;
             };
+          };
+        }
+        // lib.optionalAttrs config.programs.noctalia-shell.enable {
+          flavor = {
+            dark = "noctalia";
+            light = "noctalia";
+          };
+        };
+        settings = {
+          mgr = {
+            ratio = [
+              1
+              3
+              6
+            ];
+            sort_by = "alphabetical";
+            sort_sensitive = false;
+            sort_dir_first = true;
+            show_hidden = true;
+            show_symlink = true;
+          };
+          preview = {
+            wrap = "yes";
+            tab_size = 2;
+            max_width = 2700;
+            max_height = 2050;
           };
         };
       };
