812035c9a885cf243ee9c4bac83c79b17f4e344a
Igor Rzegocki <igor@rzegocki.pl>
2026-05-20 11:58:32 +0200

    fix(bluetooth): kernel regression in mediatek drivers

--- modules/hosts/liadtop/devices.nix
+++ modules/hosts/liadtop/devices.nix
@@ -17,6 +17,18 @@ _: {
         i2c.enable = true;
       };
 
+      # mediatek bluetooth regression patch, waiting for it, to be merged upstream
+      # more info: https://github.com/NixOS/nixpkgs/issues/521528
+      boot.kernelPatches = [
+        {
+          name = "Bluetooth: btmtk: accept too short WMT FUNC_CTRL events";
+          patch = pkgs.fetchurl {
+            url = "https://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git/patch/?id=162b1adeb057d28ad84fd8a03f3c50cf08db5c62";
+            hash = "sha256-ij0hQmC0U++AdXWQy6nycnDe6z4yaMoQIrSiLal5DHc=";
+          };
+        }
+      ];
+
       networking = {
         networkmanager.enable = true;
         hostName = "liadtop";
