d43177c160b39690172d4a316281999df79cbd3c
Igor Rzegocki <igor@rzegocki.pl>
2026-06-07 22:44:15 +0200

    chore(deps): lock file maintenance

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
     deploy-rs.url = "github:serokell/deploy-rs";
     disko.url = "github:nix-community/disko";
     git-hooks.url = "github:cachix/git-hooks.nix";
-    home-manager.url = "github:nix-community/home-manager/release-26.05";
+    home-manager.url = "github:nix-community/home-manager";
     krewfile.url = "github:brumhard/krewfile";
     nix-index-database.url = "github:nix-community/nix-index-database";
     nixos-anywhere.url = "github:nix-community/nixos-anywhere";
